16b806900a18b46d875cab6649cfc37ffa0a3c27
shidel.dev <joeshidel@gmail.com>
2014-01-26 18:36:40 -0600

    populates  data into fake collection

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+//make board collection
+//board will be 4 x 6
+
 var Card = Backbone.Model.extend({
 
 });
@@ -7,17 +10,28 @@ var Card = Backbone.Model.extend({
 
 
 
-var reddit_fetch = function(){
+var imgur_fetch = function(){
    $.ajax({
      type:'GET',
      url: "https://api.imgur.com/3/gallery/r/pics/top/1",
      headers:{
         'Authorization':'Client-ID ' + 'e0a49fd55972ffa'
      },
-     success: function(data){
-
-        console.log(data)
-     }
+     success: function(data){card_maker(data)}
     });
-}
+};
+
+
+
+
+var card_maker = function(images){ 
+   var fakeCollection = [];
+   for (var i = 0; i < images.data.length; i++){
+        console.log(images.data[i].link);
+   };
+
+   console.log(fakeCollection);
+
+   
+};
 
