3b1d741676f101604a836e0fe785c3a7d7f06062
shidel.dev <joeshidel@gmail.com>
2014-01-27 22:10:23 -0600

    deck factory method

--- js/app.js
+++ js/app.js
@@ -5,76 +5,58 @@ var Card = Backbone.Model.extend({
 
 });
 
-var Deck = new Backbone.Collection({
-    model: Card,
-    seed: function(){
-    $.ajax({
-     type:'GET',
-     url: "https://api.imgur.com/3/gallery/r/pics/top/1",
-     headers:{
-        'Authorization':'Client-ID ' + 'e0a49fd55972ffa'
-     },
-     success: function(data){card_maker(data)}
-    });
-
-    },
+var Deck = Backbone.Collection.extend({
+    model: Card
 });
 
 var Board = Backbone.View.extend({
+    initialize: function() {
+        console.log('ok')
+        var that = this;
+        this._cardViews = [];
 
-  tagName: "div",
+        this.collection.each(function(donut) {
+            that._cardViews.push(new CardView({
+                model: Deck,
+                tagName: 'img'
+            }));
+        });
+    },
 
-  className: "container",
-
-  events: {
-   
-  },
-
-  render: function() {
-    ...
-  }
+    collection: Deck,
+    el: $('#container')[0]
 
 });
 
-var Card = Backbone.View.extend({
+var CardView = Backbone.View.extend({
 
-  tagName: "img",
-
-  className: "card",
-
-  events: {
-   "click .submit": "render"
-  },
-
-  
-  render: function() {
-    
-  }
 
 });
 
 
-var imgur_fetch = function(){
-   // $.ajax({
-   //   type:'GET',
-   //   url: "https://api.imgur.com/3/gallery/r/pics/top/1",
-   //   headers:{
-   //      'Authorization':'Client-ID ' + 'e0a49fd55972ffa'
-   //   },
-   //   success: function(data){card_maker(data)}
-   //  });
+var imgur_fetch = function() {
+    $.ajax({
+        type: 'GET',
+        url: "https://api.imgur.com/3/gallery/r/pics/top/1",
+        headers: {
+            'Authorization': 'Client-ID ' + 'e0a49fd55972ffa'
+        },
+        success: function(data) {
+            card_maker(data)
+        }
+    });
 };
 
 
 
 
-var card_maker = function(images){ 
-   var fakeCollection = [];
-   for (var i = 0; i < 12; i++){
-        var card_inst = new Card({href:images.data[i].link})
-        Deck.add(card_inst)
+var card_maker = function(images) {
+    var tempCollection = [];
+    for (var i = 0; i < 12; i++) {
+        tempCollection.push({
+            href: images.data[i].link
+        })
     };
-   console.log(Deck.models);
+    var deck = new Deck(tempCollection);
+    console.log(deck)
 };
-
-
