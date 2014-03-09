c692ad8a0e6ed7df6b8fd2782bd8a126f61c6299
shidel.dev <joeshidel@gmail.com>
2014-03-08 18:10:12 -0600

    linted code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 var Card = Backbone.Model.extend({
   gameLogic:function(target){
-    var pair = this.collection.pair
+    var pair = this.collection.pair;
     if (pair.length < 1){
       pair.push(this);
-      target.firstPick()
+      target.firstPick();
     }else if(pair.length === 1){
       pair.push(this);
       if (this.isMatch(pair)){
@@ -11,7 +11,7 @@ var Card = Backbone.Model.extend({
         this.collection.remove(pair[1]);
         pair.length = 0;
       }else{
-        target.secondPick(_.clone(pair))
+        target.secondPick(_.clone(pair));
         pair.length = 0;
       }
     }  
@@ -26,11 +26,10 @@ var Card = Backbone.Model.extend({
 var Deck = Backbone.Collection.extend({
   model:Card,
   pair:[]
-})
+});
 
 
 var subreddit;
-var picks = [];
 
 var Board = Backbone.View.extend({
 
@@ -48,8 +47,8 @@ var Board = Backbone.View.extend({
       },
       success: function(res) {
         var trimedObj = _.map(res.data.slice(0, 12), function(image) {
-          return {link: image.link, imgurID: image.id}
-        })
+          return {link: image.link, imgurID: image.id};
+        });
         that.collection.add(trimedObj);
         that.collection.add(trimedObj);
         window.test = that.render(shuffle(that.collection));
@@ -60,11 +59,11 @@ var Board = Backbone.View.extend({
 
   render: function(shuffColl) {
     var that = this;
-    _(shuffColl.models).each(function(card, i) {
+    _(shuffColl.models).each(function(card) {
         $(that.el).append(new CardView({
           model: card
-        }).render().el)
-    })
+        }).render().el);
+    });
     return this;
   }
 });
@@ -81,11 +80,11 @@ var CardView = Backbone.View.extend({
   },
 
   render: function() {
-    this.$el.html("<img class='card' src='img/card.png'></img>")
+    this.$el.html("<img class='card' src='img/card.png'></img>");
     return this;
   },
 
-  sendAction: function(event){
+  sendAction: function(){
     this.model.gameLogic(this);
   },
 
@@ -103,25 +102,24 @@ var CardView = Backbone.View.extend({
     this[1].trigger("wrong");
   },
 
-
   wrongPick: function(){
     this.$el.attr("class","wrong");
-    var that = this
+    var that = this;
     setTimeout(function(){
       that.$el.removeClass("wrong");
-    },500)
+    },500);
   },
 
   match:function(){ 
     this.$el.children("img").remove();
-    this.$el.attr("class","")
+    this.$el.attr("class","");
   },
 
   showImage:function(imageHref,callback){
     $.fancybox.open({
       href: imageHref,
       afterClose:callback
-    })
+    });
   }
 
 
@@ -130,10 +128,10 @@ var CardView = Backbone.View.extend({
 
 $(document).ready(function() {
   $(".fancybox").fancybox();
-  $("#request").click(loadUrls)
+  $("#request").click(loadUrls);
   $("#subreddit").keyup(function(e) {
     if (e.keyCode == 13) {
-      loadUrls()
+      loadUrls();
     }
   });
 
@@ -141,19 +139,19 @@ $(document).ready(function() {
 var success = ["http://31.media.tumblr.com/tumblr_m61zjnHB3o1qfw2dno1_400.gif",
 "http://25.media.tumblr.com/952f0af42c4f854875606879aa87fd3c/tumblr_mhnw8krEnm1s4zq2io1_500.gif",
 "http://whatgifs.com/wp-content/uploads/2012/03/funny-gifs-winning.gif"
-]
+];
 
-var loadUrls = _.once(createBoard)
+var loadUrls = _.once(createBoard);
 
 function createBoard() {
-  subreddit = $('#subreddit').val()
-  var b = new Board({
-    collection: new Deck,
+  subreddit = $('#subreddit').val();
+  new Board({
+    collection: new Deck(),
     el: "#container"
-  })
+  });
 }
 
 function shuffle(o) {
   for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
-};
+}
